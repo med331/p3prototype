8c875f8cfdbd3d65de34ffeb392f3c25d4b62554
kuff <petergl@pm.me>
2019-10-28 11:27:05 +0100

    Add ability for Algorithm children to keep track of objects in the frame

--- algorithm.py
+++ algorithm.py
@@ -5,13 +5,19 @@ class Algorithm:  # abstract class
     def __init__(self, identifier='frame'):
         self.frame = None
         self.identifier = identifier
+        # the implemented algorithm must maintain a list of all objects in the frame
+        self.objects_in_frame = []
+
+    def _update_state(self, objects_in_frame):
+        self.objects_in_frame = objects_in_frame
 
     def _process(self, frame):  # the "_" in the name means this method should be treated as protected!
         ''' abstract method, the one you should implement '''
         raise NotImplementedError
 
     def process_frame(self, frame):
-        self.frame = self._process(frame)
+        self.frame, objects = self._process(frame)
+        self._update_state(objects)
         return self.frame
 
     def display_current_frame(self):
