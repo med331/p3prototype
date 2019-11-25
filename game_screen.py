5da390eb18517d16999c59b3688a34aa533b4683
kuff <16867891+kuff@users.noreply.github.com>
2019-11-25 10:13:27 +0100

    Make GameScreen inherit form Game instead of declaring it as field

--- game_screen.py
+++ game_screen.py
@@ -2,27 +2,26 @@
         GameScreen must define Game-related GUI elements and handle keyboard input (not webcam gesture detection)
 """
 
-from screens import Screen
+from screen import Screen
 from game import Game
 
 
-class GameScreen(Screen):
+class GameScreen(Game, Screen):  # GameScreen inherits from both Screen and Game
 
     def __init__(self):
-        super(GameScreen, self).__init__()  # calling Screen constructor
-        self.game = Game()
+        super(GameScreen, self).__init__()  # calling all parent constructors
 
     def show(self):  # implementing abstract method from Screen
         # TODO: Implement this
-        if self.game.hasFinished:
+        if self.hasFinished:
             print("Game has finished!")
         else:
             print("Game is running!")
 
-    def buttonPress(self, button):  # implementing abstract method from Screen
+    def button_press(self, button):  # implementing abstract method from Screen
         # if "q" is pressed, stop the game, otherwise start it
         if button != "q":
-            if self.game.hasFinished:
-                self.game.start()
+            if self.hasFinished:
+                self.start()
         else:
-            self.game.stop()
+            self.stop()
--- screen_manager.py
+++ screen_manager.py
@@ -15,7 +15,7 @@ class ScreenManager:
         self._activeScreenIndex = index
 
     def button_press(self, button):
-        self._screens[self._activeScreenIndex].buttonPress(button)
+        self._screens[self._activeScreenIndex].button_press(button)
 
     def show_active_screen(self):
         self._screens[self._activeScreenIndex].show()
