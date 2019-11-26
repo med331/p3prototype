3a726d8fe016f8ac4795b76d227543fe8b5c958f
kuff <16867891+kuff@users.noreply.github.com>
2019-11-26 11:59:07 +0100

    Explore GestureEngine

--- src/gesture_engine.py
+++ src/gesture_engine.py
@@ -1,13 +1,24 @@
 """
     GestureEngine must analyze a webcam for user gestures
 """
+import sys
 
 
 class GestureEngine:
 
     def __init__(self):
-        self.twoHandsInFrame = False
+        self.hands = []
+        self.distance_between_hands = sys.maxsize
+        self.middle_point = (sys.maxsize, sys.maxsize)
 
     def process_frame(self):
         # TODO: process frame
         print("Frame processing happened!")
+
+
+class Hand:
+
+    def __init__(self, x, y, width, height):
+        self.x, self.y = x, y
+        self.width, self.height = width, height
+        # self.middleX, self.middleY = middle_x, middle_y
