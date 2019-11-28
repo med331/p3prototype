617e73865087bf3fd197e6fe7c4778bae5bb8a39
steffan christensen <stefc5949@gmail.com>
2019-11-28 11:02:38 +0100

    Port algorithm to gesture_engine

---
+++ src/test_gestureEngine.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+from src.gesture_engine import *
+import cv2
+
+class TestGestureEngine(TestCase):
+
+    def test_process_frame(self):
+        cap = cv2.VideoCapture(0)
+        cap.set(3, 800)
+        cap.set(4, 600)
+        while(True):
+            frame = cap.read()[1]
+            frame2 = GestureEngine().process_frame(frame)
+            cv2.imshow('Frame', frame2)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        self.fail()
